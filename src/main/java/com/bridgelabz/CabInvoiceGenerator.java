03de97da1b2e94aa7db6a3da9e71d8d78c0915ba
vinayakgarole <vinayakgarole10@gamil.com>
2022-12-17 11:57:21 +0530

    Added: Logic for multiple ride

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -5,7 +5,24 @@ public class CabInvoiceGenerator {
     private static final int MINIMUM_COST_PER_KILOMETER = 10;
     private static final int COST_PER_TIME = 1;
 
+
     public static double calculateFare(double distance, int time) {
         return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+    }
+
+    public double calculateFareForMultipleRides(Ride[] rides) {
+
+        double totalFare = 0.0;
+
+        for (Ride ride : rides) {
+            totalFare = calculateFare(ride.getDistance(), ride.getTime());
+        }
+        return totalFare;
+    }
+
+    public static void main(String[] args) {
+
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        System.out.println(cabInvoiceGenerator.calculateFare(5, 20) + " Rs");
     }
 }
--- src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
@@ -13,4 +13,16 @@ public class CabInvoiceGeneratorTest {
         double fare = cabInvoiceGenerator.calculateFare(distance, time);
         Assertions.assertEquals(50, fare);
     }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalOfTotalFare() {
+        Ride[] rides = {new Ride(4.0, 5),
+                new Ride(5.0, 10),
+                new Ride(0.1, 1),
+                new Ride(20, 60)
+        };
+        CabInvoiceGenerator invoiceService = new CabInvoiceGenerator();
+        double totalFare = invoiceService.calculateFareForMultipleRides(rides);
+        Assertions.assertEquals(260, totalFare, 0);
+    }
 }
