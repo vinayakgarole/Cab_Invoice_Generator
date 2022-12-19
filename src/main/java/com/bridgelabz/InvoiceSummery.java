bb2e9513c1591092383d2fb97ffe132bd58b3268
vinayakgarole <vinayakgarole10@gamil.com>
2022-12-19 23:46:17 +0530

    Added: Logic for invoice service

--- src/main/java/com/bridgelabz/InvoiceSummery.java
+++ src/main/java/com/bridgelabz/InvoiceSummery.java
@@ -7,19 +7,47 @@ public class InvoiceSummery {
     int numberOfRides;
     double totalFare;
     double avgFare;
+    private double invoiceSummery;
 
+    /**
+     * create a default constructor name as InvoiceSummary
+     */
+    public InvoiceSummery() {
+
+    }
+
+    /**
+     *  create a parameterized constructor name as InvoiceSummary
+     * @param numberOfRides in cab invoice generator
+     * @param totalFare in cab invoice generator
+     */
     public InvoiceSummery(int numberOfRides, double totalFare) {
+        /**
+         *  The "this" keyword is used to refer to the current object.
+         */
         this.numberOfRides = numberOfRides;
         this.totalFare = totalFare;
     }
 
+    /**
+     * create a parameterized constructor name as InvoiceSummary
+     * @param numberOfRides in cab invoice generator
+     * @param totalFare in cab invoice generator
+     * @param type in cab invoice generator
+     */
     public InvoiceSummery(int numberOfRides, double totalFare, String type) {
-
+        /**
+         *  The "this" keyword is used to refer to the current object.
+         */
         this.numberOfRides = numberOfRides;
         this.totalFare = totalFare;
     }
 
-    public InvoiceSummery getInvoiceSummery() {
+    /**
+     * Average Fare Per Ride
+     * @return avgFare
+     */
+    public InvoiceSummery getInvoiceSummary() {
         /**
          *  The "this" keyword is used to refer to the current object.
          *  formula = avgFare =totalFare / numberOfRides
@@ -28,6 +56,13 @@ public class InvoiceSummery {
         return this;
     }
 
+    /**
+     * Create a boolean type method name as equals
+     * to compare objects we should have hashcode method else they
+     * create a two different instances so creating equals() and hashcode()
+     * @param o object
+     * @return number of rides
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -36,8 +71,21 @@ public class InvoiceSummery {
         return numberOfRides == that.numberOfRides && Double.compare(that.totalFare, totalFare) == 0 && Double.compare(that.avgFare, avgFare) == 0;
     }
 
+    /**
+     * overriding the toString() method
+     * @return firstname , numberOfRides ,totalFare, avgFare
+     *
+     */
     @Override
     public int hashCode() {
         return Objects.hash(numberOfRides, totalFare, avgFare);
+    }
+
+    public double getInvoiceSummery() {
+        return invoiceSummery;
+    }
+
+    public void setInvoiceSummery(double invoiceSummery) {
+        this.invoiceSummery = invoiceSummery;
     }
 }
--- src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
@@ -7,22 +7,48 @@ public class CabInvoiceGeneratorTest {
 
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
-        double distance = 4.0;
-        int time = 10;
-        double fare = cabInvoiceGenerator.calculateFare(distance, time);
-        Assertions.assertEquals(50, fare);
+        CabInvoiceGenerator invoiceService = new  CabInvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double totalFare = invoiceService.calculateFare(distance, time);
+        Assertions.assertEquals(25, totalFare, 0);
+    }
+
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinFare() {
+        CabInvoiceGenerator invoiceService = new  CabInvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double totalFare = invoiceService.calculateFare(distance, time);
+        Assertions.assertEquals(5.0, totalFare, 0);
     }
 
     @Test
     public void givenMultipleRides_ShouldReturnTotalOfTotalFare() {
-        Ride[] rides = {new Ride(4.0, 5),
+        Ride[] rides = {new Ride(2.0, 5),
                 new Ride(5.0, 10),
                 new Ride(0.1, 1),
                 new Ride(20, 60)
         };
-        CabInvoiceGenerator invoiceService = new CabInvoiceGenerator();
+        CabInvoiceGenerator invoiceService = new  CabInvoiceGenerator();
         double totalFare = invoiceService.calculateFareForMultipleRides(rides);
         Assertions.assertEquals(260, totalFare, 0);
+    }
+    @Test
+    public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1),
+        };
+        CabInvoiceGenerator invoiceService = new CabInvoiceGenerator();
+        InvoiceSummery invoiceSummery = invoiceService.invoiceSummaryCalculation(rides);
+        InvoiceSummery expectedInvoices = new InvoiceSummery(2, 30.0);
+        Assertions.assertEquals(expectedInvoices.getInvoiceSummery(), invoiceSummery.getInvoiceSummery());
+    }
+    @Test
+    public void givenUserId_ShouldReturnInvoiceSummary() {
+        CabInvoiceGenerator invoiceService = new CabInvoiceGenerator();
+        InvoiceSummery invoiceSummery = invoiceService.getInvoice(1);
+        InvoiceSummery expectedInvoices = new InvoiceSummery(2, 30);
+        Assertions.assertEquals(expectedInvoices.getInvoiceSummery(), invoiceSummery.getInvoiceSummery());
     }
 }
